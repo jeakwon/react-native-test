016a69962465f62b3a7950bcd904d4fc4d8cd1ea
jeakwon <onlytojay@gmail.com>
2019-07-14 15:59:00 +0900

    card, input setting

--- firstapp/App.js
+++ firstapp/App.js
@@ -1,21 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-elements';
+import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform
+} from 'react-native'
+import { Button } from 'react-native-elements'
 
-export default function App() {
+const { height, width } = Dimensions.get('window')
+
+export default function App () {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <Button title="Hello" icon={{ name : 'code'}} />
+      <StatusBar barStyle='light-content' />
+      <Text style={styles.title}>Kawai To Do</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder={'New To Do'} />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F23657',
+    alignItems: 'center'
+    // justifyContent: 'center'
   },
-});
+  title: {
+    color: 'white',
+    fontSize: 30,
+    marginTop: 50,
+    marginBottom: 50,
+    fontWeight: '200'
+  },
+  card: {
+    backgroundColor: 'white',
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgb(50, 50, 50)',
+        shadowOpacity: 50,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  }
+})
